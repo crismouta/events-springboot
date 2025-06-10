d8072b0eff9373c3587290770f446cc146155d37
Cris Mouta <82060703+crismouta@users.noreply.github.com>
2025-06-10 15:32:06 +0200

    add ResponseEntity

--- src/main/java/com/femcoders/events/controllers/EventController.java
+++ src/main/java/com/femcoders/events/controllers/EventController.java
@@ -2,6 +2,8 @@ package com.femcoders.events.controllers;
 
 import com.femcoders.events.models.Event;
 import com.femcoders.events.services.EventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +20,15 @@ public class EventController {
     }
 
     @GetMapping("/events")
-    public List<Event> getAllEvents() {
-        return eventService.getAllEvents();
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> events = eventService.getAllEvents();
+        return new ResponseEntity<List<Event>>(events, HttpStatus.OK);
+        //return eventService.getAllEvents();
     }
 
     @PostMapping("/events")
-    public void addEvent(@RequestBody Event newEvent) {
-        eventService.addEvent(newEvent);
+    public ResponseEntity<Event> addEvent(@RequestBody Event newEvent) {
+        Event createdEvent = eventService.addEvent(newEvent);
+        return new ResponseEntity<Event>(createdEvent, HttpStatus.CREATED);
     }
 }
